c26d903521c37d9528e61639c3c45acb93bea267
gumidung <zenmatsu93@gmail.com>
2016-12-02 17:09:53 +0700

    popup get data

--- list/js/list.js
+++ list/js/list.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
             tableContent += '<tr><td><p class="col-1-content">'+json.List[i].No+curPage+'</p></td> '+
                 '<td><a href="#" target="_blank" class="link"><p class="col-2-content">'+json.List[i].Content+curPage+'</p></a></td>' +
                 '<td><p class="col-3-c">'+json.List[i].Name+curPage+'</p></td>' +
-                '<td><a href="#" target="_blank"><div class="img-detail btn"><img src="images/detail.png"><p>詳細を見る</p></div></a></td>' +
+                '<td><div class="img-detail btn popup-btn"><img src="images/detail.png"><p>詳細を見る</p></div></td>' +
                 '</tr>';
         }
         $("#table-paging").empty();
@@ -47,7 +47,7 @@ $(document).ready(function(){
             tableContent += '<tr><td><p class="col-1-content">'+json.List[i].No+curPage+'</p></td> '+
                 '<td><a href="#" target="_blank" class="link"><p class="col-2-content">'+json.List[i].Content+curPage+'</p></a></td>' +
                 '<td><p class="col-3-c">'+json.List[i].Name+curPage+'</p></td>' +
-                '<td><a href="#" target="_blank"><div class="img-detail btn"><img src="images/detail.png"><p>詳細を見る</p></div></a></td>' +
+                '<td><div class="img-detail btn popup-btn"><img src="images/detail.png"><p>詳細を見る</p></div></td>' +
                 '</tr>';
         }
         $("#table-paging").empty();
@@ -66,40 +66,96 @@ $(document).ready(function(){
     $get_popup_info.each(function() {
         var $this_popup_info = $(this);
         fix_margin($this_popup_info);
-        $this_popup_info.find(".click-fix-size").click(function() {
-            if($this_popup_info.hasClass("popup-expand-active")) {
-                $this_popup_info.removeClass("popup-expand-active popup--active");
-            }
-            else {
-                $this_popup_info.toggleClass("popup--active");
-                fix_margin($this_popup_info);
-            }
-        });
-
-        $this_popup_info.find(".click-expand").click(function() {
-            $this_popup_info.addClass("popup-expand-active popup--active");
-            fix_margin($this_popup_info);
-            $get_popup_info.not($this_popup_info).removeClass("popup--active");
-
-            if($get_popup_info.not($this_popup_info).hasClass("popup-expand-active")) {
-                $get_popup_info.not($this_popup_info).removeClass("popup-expand-active");
-            }
-        });
-
-        $this_popup_info.find(".click-expand-02").click(function() {
-            $this_popup_info.removeClass("popup-expand-active");
-        });
-
 
     });
 
 
+    $(".popup-info__body__content").load("step/index.html");
+
+
+    //$(".popup-btn").each(function() {
+    //    $(this).click(function() {
+    //    })
+    //});
+
+    $(".detail-content").on('click','.popup-btn',function() {
+        var $get_this_click = $(this);
+        var get_number = guid();
+
+        var data_array = {
+            get_id: $(this).parents("tr").find(".col-1-content").text(),
+            get_title: $(this).parents("tr").find(".col-2-content").text(),
+            get_dr: $(this).parents("tr").find(".col-3-content").text(),
+            get_budget: $(this).parents("tr").find(".col-4-content").text()
+        };
+
+        var $get_this_popup_data = $(".popup-info-wrap").find("#"+$get_this_click.attr("tag"));
+        if($get_this_popup_data.length > 0) {
+            $get_this_popup_data.addClass("popup--active");
+            fix_margin($get_this_popup_data);
+        } else {
+            $get_this_click.attr("tag",get_number);
+            var $body = $(".clone-item").clone().removeClass("clone-item").attr('id',get_number).prependTo(".popup-info-wrap");
+            //$body.find("#get-id").text("hello");
+        }
+    });
+
+
+
+    var popup_info_wrap = $(".popup-info-wrap");
+    popup_info_wrap.on('click','.click-close', function() {
+        var $this_popup_info = $(this).parents(".popup-info-wrap--pop");
+        $this_popup_info.remove();
+    });
+
+    popup_info_wrap.on('click','.click-fix-size', function() {
+        var $this_popup_info = $(this).parents(".popup-info-wrap--pop");
+        if($this_popup_info.hasClass("popup-expand-active")) {
+            $this_popup_info.removeClass("popup-expand-active popup--active");
+        }
+        else {
+            $this_popup_info.toggleClass("popup--active");
+            fix_margin($this_popup_info);
+        }
+
+    });
+
+    popup_info_wrap.on('click','.click-expand', function() {
+        var  $this_get_popup_info = $(this).parents(".popup-info-wrap");
+        var $get_child = $this_get_popup_info.find(".popup-info-wrap--pop");
+
+        var $this_popup_info = $(this).parents(".popup-info-wrap--pop");
+        $this_popup_info.addClass("popup-expand-active popup--active");
+        fix_margin($this_popup_info);
+        $get_child.not($this_popup_info).removeClass("popup--active");
+
+        if($get_child.not($this_popup_info).hasClass("popup-expand-active")) {
+            $get_child.not($this_popup_info).removeClass("popup-expand-active");
+        }
+    });
+
+    popup_info_wrap.on('click','.click-expand-02', function() {
+        var $this_popup_info = $(this).parents(".popup-info-wrap--pop");
+        $this_popup_info.removeClass("popup-expand-active");
+    });
 });
 
 function fix_margin(target) {
     target.css("margin-top",-(target.height()));
 }
 
+
 $(window).resize(function() {
 
 })
+
+//random key
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
