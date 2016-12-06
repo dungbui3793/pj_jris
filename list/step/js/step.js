69bd0eca4f5ad91fcc950b625cb9927330fe8bad
gumidung <zenmatsu93@gmail.com>
2016-12-06 10:38:39 +0700

    step popup

--- list/step/js/step.js
+++ list/step/js/step.js
@@ -6,7 +6,7 @@ $(window).ready(function() {
 
     $(".table-1 .table-1__row .table-1__row--02 > *").each(function() {
         if($(this).height() < $get_max) {
-            $(this).css("line-height",$get_max+'px');
+            $(this).css("line-height",$get_max+"px");
         }
     });
 
@@ -17,74 +17,86 @@ $(window).ready(function() {
         max: 5,
         step: 0.1
     });
-    var score1 = localStorage.getItem("score-1");
-    var score2 = localStorage.getItem("score-2");
-    var score3 = localStorage.getItem("score-3");
-    var score4 = localStorage.getItem("score-4");
-    var score5 = localStorage.getItem("score-5");
-    var score6 = localStorage.getItem("score-6");
-    var comment1 = localStorage.getItem("comment-1");
-    var comment2 = localStorage.getItem("comment-2");
-    var comment3 = localStorage.getItem("comment-3");
-    var comment4 = localStorage.getItem("comment-4");
-    var comment5 = localStorage.getItem("comment-5");
-    var comment6 = localStorage.getItem("comment-6");
-    var commentAll = localStorage.getItem("comment-all");
-    if (score1 != null && score2 != null && score3 != null && score4 != null && score5 != null &&
-        score6 != null && comment1 != null && comment2 != null && comment3 != null &&
-        comment4 != null && comment5 != null && comment6 != null  && commentAll != null) {
-        $("#score-1").slider({value:score1});
-        $( "#txt-score-1").text(score1);
-        $("#score-2").slider({value:score2});
-        $( "#txt-score-2").text(score2);
-        $("#score-3").slider({value:score3});
-        $( "#txt-score-3").text(score3);
-        $("#score-4").slider({value:score4});
-        $( "#txt-score-4").text(score4);
-        $("#score-5").slider({value:score5});
-        $( "#txt-score-5").text(score5);
-        $("#score-6").slider({value:score6});
-        $( "#txt-score-6").text(score6);
-        $('.txt-1').val(comment1);
-        $('.txt-2').val(comment2);
-        $('.txt-3').val(comment3);
-        $('.txt-4').val(comment4);
-        $('.txt-5').val(comment5);
-        $('.txt-6').val(comment6);
-        $('.txt-comment').val(commentAll);
-        localStorage.clear();
-    }
-    $(".submit-btn").click(function(){
-        $('.alert-7').removeClass('active');
-        $('.alert-table').removeClass('active');
-        if ($('.txt-comment').val()=="") {
+    $(".footer__btn--submit.btn-step-01").click(function() {
+        var score1 = $("#txt-score-1").text();
+        var score2 = $("#txt-score-2").text();
+        var score3 = $("#txt-score-3").text();
+        var score4 = $("#txt-score-4").text();
+        var score5 = $("#txt-score-5").text();
+        var score6 = $("#txt-score-6").text();
+        var comment1 = $(".txt-1").val();
+        var comment2 = $(".txt-2").val();
+        var comment3 = $(".txt-3").val();
+        var comment4 = $(".txt-4").val();
+        var comment5 = $(".txt-5").val();
+        var comment6 = $(".txt-6").val();
+        var commentAll = $(".txt-comment").val();
+
+        if(commentAll == "") {
             $('.alert-7').addClass('active');
             $('.alert-table').addClass('active');
-            return;
-        }
-        localStorage.setItem("score-1", $('#score-1').slider("option", "value"));
-        localStorage.setItem("score-2", $('#score-2').slider("option", "value"));
-        localStorage.setItem("score-3", $('#score-3').slider("option", "value"));
-        localStorage.setItem("score-4", $('#score-4').slider("option", "value"));
-        localStorage.setItem("score-5", $('#score-5').slider("option", "value"));
-        localStorage.setItem("score-6", $('#score-6').slider("option", "value"));
-        localStorage.setItem("comment-1", $('.txt-1').val());
-        localStorage.setItem("comment-2", $('.txt-2').val());
-        localStorage.setItem("comment-3", $('.txt-3').val());
-        localStorage.setItem("comment-4", $('.txt-4').val());
-        localStorage.setItem("comment-5", $('.txt-5').val());
-        localStorage.setItem("comment-6", $('.txt-6').val());
-        localStorage.setItem("comment-all", $('.txt-comment').val());
-        var jumpTo = "../step2/index.html"
-        if(ltie9()){
-            var referLink = document.createElement('a');
-            referLink.href = jumpTo;
-            document.body.appendChild(referLink);
-            referLink.click();
-        }else{
-            location.href = jumpTo;
+        } else {
+            $(".step-01").fadeOut(300);
+            $(".step-02").fadeIn(300);
+            $(".step-wrap__01__footer .btn-step-02").css("display","inline-block");
+            $(".step-wrap__01__footer .btn-step-01").css("display","none");
+
+            $(".score-1").text(score1);
+            $(".score-2").text(score2);
+            $(".score-3").text(score3);
+            $(".score-4").text(score4);
+            $(".score-5").text(score5);
+            $(".score-6").text(score6);
+            $(".comment-1").text(comment1);
+            $(".comment-2").text(comment2);
+            $(".comment-3").text(comment3);
+            $(".comment-4").text(comment4);
+            $(".comment-5").text(comment5);
+            $(".comment-6").text(comment6);
+            $(".comment-all").text(commentAll);
+
+            //count score
+            var realScore1 = ((score1*50)/100);
+            $('.real-score-1').text(realScore1.toFixed(1));
+            var realScore2 = ((score2*20)/100);
+            $('.real-score-2').text(realScore2.toFixed(1));
+            var realScore3 = ((score3*5)/100);
+            $('.real-score-3').text(realScore3.toFixed(1));
+            var realScore4 = ((score4*10)/100);
+            $('.real-score-4').text(realScore4.toFixed(1));
+            var realScore5 = ((score5*10)/100);
+            $('.real-score-5').text(realScore5.toFixed(1));
+            var realScore6 = ((score6*5)/100);
+            $('.real-score-6').text(realScore6.toFixed(1));
+            var finalScore = ((realScore1) + (realScore2) + (realScore3) + (realScore4) + (realScore5) + (realScore6)).toFixed(1);
+            $('.final-score').text(finalScore);
         }
     });
+
+    // CLICK TO STEP 3
+    $(".footer__btn--submit.btn-step-02").click(function() {
+        $(".step-02").fadeOut(300);
+        $(".step-03").fadeIn(300);
+        $(".step-wrap__01__footer .btn-step-02").css("display","none");
+        $(".footer__btn--submit.btn-step-03").css("display","inline-block");
+    });
+
+    //CLICK TO CLOSE
+    $(".footer__btn--back.btn-step-01").click(function() {
+        $(this).parents(".popup-info-wrap--pop").remove();
+    });
+    $(".footer__btn--submit.btn-step-03").click(function() {
+       $(this).parents(".popup-info-wrap--pop").remove();
+    });
+
+    //CLICK BACK
+    $(".footer__btn--back.btn-step-02").click(function() {
+        $(".step-02").fadeOut(300);
+        $(".step-01").fadeIn(300);
+        $(".step-wrap__01__footer .btn-step-01").css("display","inline-block");
+        $(".step-wrap__01__footer .btn-step-02").css("display","none");
+    });
+
     $( "#score-1" ).on( "slide", function( event, ui ) {
         $( "#txt-score-1").text(ui.value.toFixed(1));
     } );
